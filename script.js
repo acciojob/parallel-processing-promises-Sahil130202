08f8ed043a0dbd51d075a2e7cd420dba7534225a
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-14 11:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" }
 ];
+
+// Create loading and error elements
+const loading = document.createElement("div");
+loading.id = "loading";
+loading.innerText = "Loading...";
+
+const error = document.createElement("div");
+error.id = "error";
+
+output.appendChild(loading);
+output.appendChild(error);
+
+
+// Function to download an image
+function downloadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+
+        img.onload = () => {
+            resolve(img);
+        };
+
+        img.onerror = () => {
+            reject(new Error("Failed to download image: " + url));
+        };
+
+        img.src = url;
+    });
+}
+
+
+// Download all images
+function downloadImages() {
+    loading.innerText = "Loading...";
+    error.innerText = "";
+    
+    Promise.all(images.map(image => downloadImage(image.url)))
+        .then((downloadedImages) => {
+            loading.innerText = "";
+
+            downloadedImages.forEach(img => {
+                output.appendChild(img);
+            });
+        })
+        .catch((err) => {
+            loading.innerText = "";
+            error.innerText = err.message;
+        });
+}
+
+downloadImages();
